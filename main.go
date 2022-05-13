cc3119c1584bdc5298a57ab280d286745e94481c
Rasil07 <rasilgrt@gmail.com>
2022-05-13 19:20:28 +0545

    cors tweaks

--- main.go
+++ main.go
@@ -6,10 +6,28 @@ import (
 	"first_api/tuts/Routes"
 	"fmt"
 
+	"github.com/gin-gonic/gin"
+
 	"github.com/jinzhu/gorm"
 )
 
 var err error
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
+}
 
 func main(){
 
@@ -21,7 +39,9 @@ func main(){
 
 	defer Config.DB.Close()
 		Config.DB.AutoMigrate(&Models.Todo{})
-
 	r := Routes.SetupRouter()
+
+	r.Use(CORSMiddleware())
+
 	r.Run()	
 }
